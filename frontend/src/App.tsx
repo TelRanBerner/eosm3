a168da934f82c939c16bf7402b570f2c2bd46c5d
Yulia Berner <lulikartik7@gmail.com>
2026-03-13 15:27:22 +0200

    reduce logout timeout to 1s

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = () => {
         try {
             await Promise.race([
                 api.post('/auth/logout'),
-                new Promise((_, reject) => setTimeout(() => reject('timeout'), 3000))
+                new Promise((_, reject) => setTimeout(() => reject('timeout'), 1000))
             ]);
         } catch {}
         setUser(null);
