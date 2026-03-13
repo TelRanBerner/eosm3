554a85197375398ff6cbf9bc88203b9dcdfff5ef
Yulia Berner <lulikartik7@gmail.com>
2026-03-13 12:22:32 +0200

    update api url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://eosm3-production.up.railway.app/api',
+    baseURL: 'https://eosm3-production-1bb1.up.railway.app/api',
     withCredentials: true,
 });
 
